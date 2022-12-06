eab5e926ff9ee4c1af24802521e3d15d3aa489df
prince475 <danielmwai475@gmail.com>
2022-12-06 23:03:02 +0300

    DOM Form manupilation done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value) //target grabs a specific input new_todo and its value after submision
+    form.reset()
+})
   // your code here
 });
+function buildToDo (Todos){
+  console.log(Todos)
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${Todos} `
+  li.appendChild(btn)
+  document.querySelector('#list').appendChild(li)
+}
+function handleDelete (e){
+  e.target.parentNode.remove()
+  }
+
+
+
+  
